361310d8a1273ee25c374038171620e4c12f2495
Gabriel <gabrielzanlorenssi@gmail.com>
2021-06-09 11:25:22 -0300

    update local

---
+++ dashboard/server.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(shinydashboard)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    
+    output$progressBox <- renderValueBox({
+        valueBox(
+            paste0(8 + input$count, " casos"), "Nos últimos 14 dias", icon = icon("list"),
+            color = "red"
+        )
+    })
+    
+    output$approvalBox <- renderValueBox({
+        valueBox(
+            "80%", "Casos ativos", icon = icon("thumbs-up", lib = "glyphicon"),
+            color = "red"
+        )
+    })
+
+
+# Download relatorio ------------------------------------------------------
+    output$downloadReport <- downloadHandler(
+        filename = function() {
+            paste('my-report', sep = '.', switch(
+                input$format, PDF = 'pdf', HTML = 'html', Word = 'docx'
+            ))
+        },
+        
+        content = function(file) {
+            src <- normalizePath('report.Rmd')
+            
+            # temporarily switch to the temp dir, in case you do not have write
+            # permission to the current working directory
+            owd <- setwd(tempdir())
+            on.exit(setwd(owd))
+            file.copy(src, 'report.Rmd', overwrite = TRUE)
+            
+            library(rmarkdown)
+            out <- render('report.Rmd', switch(
+                input$format,
+                PDF = pdf_document(), HTML = html_document(), Word = word_document()
+            ))
+            file.rename(out, file)
+        }
+    )
+
+#--    
+})
