4d8f61d1b58e8cd6cafe9cba12dcdf78e5377385
gabrielzanlorenssi <gabrielzanlorenssi@gmail.com>
2021-06-09 22:18:37 -0300

    update local

--- dashboard/ui.R
+++ dashboard/ui.R
@@ -6,8 +6,11 @@ library(tidyverse)
 #--- data
 escolas <- read_csv("data/escola_segura.csv")
 
-texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacus ante, mattis sit amet rutrum et, ornare ac nibh. Fusce et faucibus purus. Sed maximus mollis quam sed ornare. Aliquam laoreet tortor et imperdiet faucibus. Nam vel diam vel sapien fermentum condimentum. Integer porta sodales risus. Nulla placerat ultrices neque id volutpat. Phasellus at massa nec diam gravida facilisis. Nulla eros elit, varius at nibh eget, accumsan sodales tortor. Donec finibus at orci eget accumsan. Praesent purus turpis, placerat sit amet ligula non, tempor aliquet nulla. Suspendisse accumsan diam euismod eros convallis, a posuere libero hendrerit. Ut congue metus non aliquam porttitor.
-Cras ultricies, nisl a sodales fermentum, nisl felis luctus ante, vitae consectetur neque nibh in ex. Cras condimentum sapien sit amet dui vestibulum cursus. Nullam quam odio, condimentum euismod nunc eget, porta sollicitudin tortor. Vivamus eget nulla nisi. Nullam sed dignissim metus, at condimentum libero. In eu rutrum nulla. Vivamus dapibus ipsum id urna pretium auctor. Phasellus turpis odio, tempus eget consectetur pellentesque, suscipit sed nibh. In ac sapien sodales, vulputate sapien at, pulvinar nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. In vitae ex hendrerit tellus interdum dignissim. Nullam volutpat nibh in molestie tincidunt. Donec iaculis dolor tellus, vel tincidunt dolor tempus in. Nulla feugiat leo vitae porta ultricies."
+options1 <- c("Caruaru (PE)", "Porto Velho (RO)", "São Paulo (SP)")
+options2 <- c("Todas as escolas", unique(escolas$`Qual o nome da sua escola? (EXEMPLOS)`))
+
+texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacus ante, mattis sit amet rutrum et, ornare ac nibh. Fusce et faucibus purus. \n Sed maximus mollis quam sed ornare. Aliquam laoreet tortor et imperdiet faucibus. Nam vel diam vel sapien fermentum condimentum. Integer porta sodales risus. Nulla placerat ultrices neque id volutpat. Phasellus at massa nec diam gravida facilisis. Nulla eros elit, varius at nibh eget, accumsan sodales tortor. Donec finibus at orci eget accumsan. Praesent purus turpis, placerat sit amet ligula non, tempor aliquet nulla. Suspendisse accumsan diam euismod eros convallis, a posuere libero hendrerit. Ut congue metus non aliquam porttitor.
+Cras ultricies, nisl a sodales fermentum, nisl felis luctus ante, vitae consectetur neque nibh in ex. Cras condimentum sapien sit amet dui vestibulum cursus. \n Nullam quam odio, condimentum euismod nunc eget, porta sollicitudin tortor. Vivamus eget nulla nisi. Nullam sed dignissim metus, at condimentum libero. In eu rutrum nulla. Vivamus dapibus ipsum id urna pretium auctor. Phasellus turpis odio, tempus eget consectetur pellentesque, suscipit sed nibh. In ac sapien sodales, vulputate sapien at, pulvinar nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. In vitae ex hendrerit tellus interdum dignissim. Nullam volutpat nibh in molestie tincidunt. Donec iaculis dolor tellus, vel tincidunt dolor tempus in. Nulla feugiat leo vitae porta ultricies."
 
 #--- dashboard
 
@@ -18,20 +21,16 @@ dashboardPage(
     #-- sidebar
     dashboardSidebar(menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
                      selectInput("variable1", "Selecione o município:",
-                                 c("Cylinders" = "cyl",
-                                   "Transmission" = "am",
-                                   "Gears" = "gear")),
+                                 options1),
                      selectInput("variable2", "Selecione a escola:",
-                                 c("Cylinders" = "cyl",
-                                   "Transmission" = "am",
-                                   "Gears" = "gear")),
+                                 options2),
                      sliderInput("slider", "Slider", 1, 100, 50),
                      menuItem("Download", tabName = "download", icon = icon("th")),
                      menuItem("Como usar", tabName = "instructions", icon = icon("dashboard"))),
     #-- body
     dashboardBody(
         tabItems(tabItem(tabName = "dashboard",
-                h2("Dashboard tab content"),
+                h2("Caruaru (PE)"),
         fluidRow(
             # A static valueBox
             valueBox(36, "Casos de covid-19 em 2021", icon = icon("credit-card")),
@@ -40,16 +39,16 @@ dashboardPage(
             valueBoxOutput("progressBox"),
             
             valueBoxOutput("approvalBox")
-        ),
-        fluidRow(
-            # Clicking this will increment the progress amount
-            box(width = 4, actionButton("count", "Increment progress"))
         )),
         tabItem(tabName = "download",
                 h2("Downloads"),
-                radioButtons('format', 'Document format', c('PDF', 'HTML', 'Word'),
+                radioButtons('format', 'Formato do documento', c('PDF', 'HTML', 'Word'),
                              inline = TRUE),
-                downloadButton("downloadReport", "Baixe um relatório do município")),
+                downloadButton("downloadReport", "Baixe um relatório do município"),
+                br(),
+                radioButtons('formatDoc', 'Formato da tabela', c('Excel', 'CSV'),
+                             inline = TRUE),
+                downloadButton("downloadDatabase", "Baixe os dados do município como tabela")),
         tabItem(tabName = "instructions",
                 h2("Como usar"),
                 p(texto))
